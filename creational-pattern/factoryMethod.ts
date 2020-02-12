b5e166c3b3443672c0e9194e8f51efc01f9eade1
Duy Tran <dqtran@axon.com>
2020-02-12 23:47:17 +0700

    factory method

--- creational-pattern/factoryMethod.ts
+++ creational-pattern/factoryMethod.ts
@@ -1,1 +1,50 @@
 
+interface Transporter {
+    delivery(): void
+}
+
+class Truck implements Transporter {
+    delivery() {
+        console.log('do something');
+    }
+}
+
+class Ship implements Transporter {
+    delivery() {
+        console.log('do something else');
+    }
+}
+
+class Logistics {
+    transporter: Transporter
+    constructor(transporter: Transporter) {
+        this.transporter = transporter
+    }
+    planDelivery() {
+
+    }
+    
+    createTransport() {
+        
+    }
+}
+
+class RoadLogistics extends Logistics { 
+    constructor() {
+        const truckTransport = new Truck();
+        super(truckTransport)
+    }
+    planDelivery() {
+        
+    }
+}
+
+class SeaLogistics extends Logistics {
+    constructor() {
+        const shipTransport = new Ship();
+        super(shipTransport)
+    }
+    planDelivery() {
+
+    }
+}
